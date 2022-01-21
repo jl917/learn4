ca8df4bef928afc44d65e4c3302599fc658d6281
JuLong <julong@wemakeprice.com>
2022-01-21 18:23:45 +0900

    fix: a

--- countPlugin/index.js
+++ countPlugin/index.js
@@ -30,3 +30,10 @@ countPlugin.register(doubleIncreasePlugin);
 countPlugin.doubleIncrease()
 
 console.log(countPlugin.value)
+
+// 1. 解决问题前首先要定义问题
+// 2. 插件架构设计绕不开的几大要素
+// 如何注入、配置、初始化插件
+// 插件如何影响系统
+// 插件输入输出的含义与可以使用的能力
+// 复数个插件之间的关系是怎么样的
